d66adf41bef19b4aac6104314998640513f9c7da
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-23 00:28:33 +0300

    add validation for the input and stage change for successful login

--- src/main/java/com/riis/controller/LoginController.java
+++ src/main/java/com/riis/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.riis.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController { 
 
@@ -18,11 +19,47 @@ public class LoginController {
     @FXML
     private void login() throws Exception {
 
+        String user = username.getText();
+        String pass = password.getText();
+        
         // Test: Print Username and Password in the console
         System.out.println("Username: " + username.getText());
         System.out.println("Password: " + password.getText());
 
-        System.out.println("Logging in");
+        if(isValidated()) {
+            if (user.equals("username") && pass.equals("password")) {
+                System.out.println("Login Successful");
+                Stage stage = (Stage) loginButton.getScene().getWindow();
+                stage.close();
+
+                SidebarController sidebarController = new SidebarController(stage);
+                sidebarController.render();
+
+            } else {
+                System.out.println("Login Failed");
+            }
+        }
+
+    }
+    
+    private boolean isValidated() {
+        
+        if (username.getText().equals("")) {
+            System.out.println("Username text field cannot be blank.");
+            username.requestFocus();
+        } else if (username.getText().length() < 5 || username.getText().length() > 25) {
+            System.out.println("Username text field cannot be less than 5 and greater than 25 characters.");
+            username.requestFocus();
+        } else if (password.getText().equals("")) {
+            System.out.println("Password text field cannot be blank.");
+            password.requestFocus();
+        } else if (password.getText().length() < 5 || password.getText().length() > 25) {
+            System.out.println("Password text field cannot be less than 5 and greater than 25 characters.");
+            password.requestFocus();
+        } else {
+            return true;
+        }
+        return false;
     }
 
 }
