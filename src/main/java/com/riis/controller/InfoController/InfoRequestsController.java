e39a92a631c0499de5a886c5d6565a775cc0d867
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-04 15:53:31 +0300

    make the requests page for all the actors dynamic

--- src/main/java/com/riis/controller/InfoController/InfoRequestsController.java
+++ src/main/java/com/riis/controller/InfoController/InfoRequestsController.java
@@ -8,12 +8,18 @@ import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 
 public class InfoRequestsController extends BaseRequestsController {
+
+    public void initialize() throws Exception{
+        requestModel.setRequestTypeText("Pending Sealed Requests");
+        requestModel.setRequestTypeComp();
+    }
     @Override
     public void getView() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/riis/fxml/Base_fxml/BaseRequests.fxml"));
 
         AnchorPane anchorPane = (AnchorPane) root;
         SidebarModel.borderPane.setCenter(anchorPane);
+        initialize();
     }
 
 }
--- src/main/java/com/riis/controller/KebeleController/KebeleRequestsController.java
+++ src/main/java/com/riis/controller/KebeleController/KebeleRequestsController.java
@@ -8,10 +8,19 @@ import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 
 public class KebeleRequestsController extends BaseRequestsController {
+
+    public void initialize() throws Exception{
+        requestModel.setRequestTypeText("Pending Approval Requests");
+        requestModel.setRequestTypeComp();
+    }
+    
+    @Override
     public void getView() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/riis/fxml/Base_fxml/BaseRequests.fxml"));
-
+        
         AnchorPane anchorPane = (AnchorPane) root;
         SidebarModel.borderPane.setCenter(anchorPane);
+        requestModel.setRequestTypeComp();
+        initialize();
     }   
 }
