eb34518b47bf93f5fd914767e265e4b90e8c37f1
sgc93 <smachewgedefaw@gmail.com>
2023-06-04 13:17:38 -0900

    create table

--- src/main/java/com/riis/controller/LoginController.java
+++ src/main/java/com/riis/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.riis.controller;
 
+import java.sql.Statement;
+import java.sql.Connection;
+
 import com.riis.auth.AuthenticationManager;
 import com.riis.controller.AdminController.AdminSidebarController;
 import com.riis.controller.InfoController.InfoSidebarController;
+import com.riis.database.DatabaseConnection;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -89,6 +93,20 @@ public class LoginController implements Controller {
         // Test: Print Username and Password in the console
         System.out.println("Username: " + username.getText());
         System.out.println("Password: " + password.getText());
+        // Connection con = DatabaseConnection.getInstance();
+        // Statement st = con.createStatement();
+        // st.executeQuery("CREATE TABLE Employee(\n" +
+        //     "username varchar(25),\n" +
+        //     "password varchar(25),\n" +
+        //     "firstName varchar(25),\n" +
+        //     "lastName varchar(25),\n" +
+        //     "middleName varchar(25),\n" +
+        //     "job varchar(25)\n" +
+        // ");");
+
+        // st.executeUpdate("INSERT INTO Employee VALUES('helen003', 'helen003', 'Helen', 'Shiferaw', 'Gemeda', 'Information Officer')");
+        // st.executeUpdate("INSERT INTO Employee VALUES('admin001', 'admin001', 'Sewlesew', 'Sewmehon', 'Yshalal', 'System Administer')");
+
         String job = AuthenticationManager.authenticate(user, pass);
 
         if (!job.isEmpty()) {
