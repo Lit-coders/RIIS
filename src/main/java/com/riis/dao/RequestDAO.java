cf1227efa59305aed576e2e43ab4cd6f603b4c4a
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-06-06 01:42:28 -0700

    create updateRequest method for RequestDAO interface

--- src/main/java/com/riis/dao/RequestDAO.java
+++ src/main/java/com/riis/dao/RequestDAO.java
@@ -2,11 +2,15 @@ package com.riis.dao;
 
 import java.util.List;
 
+
 import com.riis.model.databasemodel.Request;
+
 
 public interface RequestDAO {
 
     List<Request> getPendingUnpaidRequests() throws Exception;
+    // ObservableList<Request> getPendingUnapproveRequests() throws Exception;
     void addRequest(Request request) throws Exception;
+    void updateRequest(Request request) throws Exception;
     
 }
