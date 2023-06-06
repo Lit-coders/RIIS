665d9814c63b5a307b7fc7616d99486dd17fb6d9
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-06-06 16:10:32 -0700

    declare the function getPendingSealedRequests

--- src/main/java/com/riis/dao/RequestDAO.java
+++ src/main/java/com/riis/dao/RequestDAO.java
@@ -12,9 +12,12 @@ public interface RequestDAO {
 
     List<Request> getPendingUnpaidRequests() throws Exception;
     ObservableList<Request> getPendingUnapprovedRequests() throws Exception;
+    ObservableList<Request> getPendingSealedRequests() throws Exception;
     void addRequest(Request request) throws Exception;
     void updateRequest(Request request) throws Exception;
     void addToCreationPayment(Request request) throws Exception;
     void approveRequest(Request request) throws Exception;
+    void addToKebeleResidentID(Request request) throws Exception;
+    
     
 }
