ad8d6c073b3a5279c4302ed2a91bc9a7ea09ea68
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-06-06 14:51:14 -0700

    add addToCreationPayment and approveRequest method

--- src/main/java/com/riis/dao/RequestDAO.java
+++ src/main/java/com/riis/dao/RequestDAO.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import com.riis.model.databasemodel.Request;
 
+import javafx.collections.ObservableList;
+
 
 public interface RequestDAO {
 
     List<Request> getPendingUnpaidRequests() throws Exception;
-    // ObservableList<Request> getPendingUnapproveRequests() throws Exception;
+    ObservableList<Request> getPendingUnapprovedRequests() throws Exception;
     void addRequest(Request request) throws Exception;
     void updateRequest(Request request) throws Exception;
+    void addToCreationPayment(Request request) throws Exception;
+    void approveRequest(Request request) throws Exception;
     
 }
