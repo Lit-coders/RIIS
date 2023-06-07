67912fbb5ec8149c78e24df4e354c66e9977ee90
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-07 03:31:36 +0300

    create dao for the employee model

--- src/main/java/com/riis/dao/EmployeeDAO.java
+++ src/main/java/com/riis/dao/EmployeeDAO.java
@@ -2,9 +2,15 @@ package com.riis.dao;
 
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.riis.model.databasemodel.Employee;
 
 public interface EmployeeDAO {
+    public List<Employee> getAllEmployees() throws SQLException;
     public Employee getEmployeeByUsername(String userName) throws SQLException;
+    public boolean addEmployee(Employee employee) throws SQLException;
+    public boolean deleteEmployee(Employee employee) throws SQLException;
+    public void captureLoginTime(String userName) throws SQLException;
+    public String getLastLogin(String userName) throws SQLException;
 }
