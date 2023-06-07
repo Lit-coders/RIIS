1729f26fb14c1981a66389310f37223680d8dd58
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-07 03:28:44 +0300

    custom date provider for last login and other functionalities

---
+++ src/main/java/com/riis/utils/DateProvider.java
@@ -0,0 +1,20 @@
+package com.riis.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateProvider {
+    public static LocalDateTime getDateTime() {
+        String now = LocalDateTime.now().toString();
+        String clean = now.substring(0, now.indexOf('T')) + " " + now.substring(now.indexOf('T') + 1, now.indexOf('.'));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime currentTime = LocalDateTime.parse(clean, formatter);
+        return currentTime;
+    }
+
+    public static String getDateTimeString() {
+        String now = LocalDateTime.now().toString();
+        String clean = now.substring(0, now.indexOf('T')) + " " + now.substring(now.indexOf('T') + 1, now.indexOf('.'));
+        return clean;
+    }
+}
