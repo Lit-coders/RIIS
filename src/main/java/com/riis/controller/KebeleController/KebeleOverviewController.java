51b233da611e75bd388b1f3af8732b872693f88c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-08 22:34:20 +0300

    implement recent activity card on all overview pages

--- src/main/java/com/riis/controller/KebeleController/KebeleOverviewController.java
+++ src/main/java/com/riis/controller/KebeleController/KebeleOverviewController.java
@@ -1,11 +1,17 @@
 package com.riis.controller.KebeleController;
 
+import java.util.List;
+
 import com.riis.controller.BaseController.BaseOverviewController;
+import com.riis.dao.RequestDAO;
+import com.riis.dao.RequestDAOImpl;
+import com.riis.model.databasemodel.Request;
 import com.riis.model.viewmodel.SidebarModel;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 public class KebeleOverviewController extends BaseOverviewController {
 
@@ -20,6 +26,15 @@ public class KebeleOverviewController extends BaseOverviewController {
         AnchorPane anchorPane = (AnchorPane) root;
         SidebarModel.borderPane.setCenter(anchorPane);
         initialize();
+    }
+
+    public void fetchRecentActivity() throws Exception {
+        RequestDAO requestDAO = new RequestDAOImpl();
+        List<Request> unpaidRequests = requestDAO.getPendingSealedRequests();  
+        for(Request request : unpaidRequests) {
+            HBox hBox = buildHBox(request);
+            overviewModel.getRecentActivityComp().getChildren().add(hBox);
+        }        
     }
 
 }
