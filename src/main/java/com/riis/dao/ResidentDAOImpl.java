8fa51b30e5a2b62add27d11e6047f14f79d50e2e
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-08 09:17:46 +0300

    add methods for database operations all and byID

--- src/main/java/com/riis/dao/ResidentDAOImpl.java
+++ src/main/java/com/riis/dao/ResidentDAOImpl.java
@@ -3,6 +3,9 @@ package com.riis.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.riis.database.DatabaseConnection;
 import com.riis.model.databasemodel.Resident;
@@ -17,10 +20,80 @@ public class ResidentDAOImpl implements ResidentDAO {
         ps.setInt(1, id);
         ResultSet resultSet = ps.executeQuery();
 
-        while(resultSet.next()) {
+        while (resultSet.next()) {
             resident.setName(resultSet.getString("Name"));
             resident.setFName(resultSet.getString("FName"));
         }
         return resident;
     }
+
+    @Override
+    public List<Resident> getAllResidents() throws Exception {
+        List<Resident> residents = new ArrayList<>();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM Resident";
+        try (PreparedStatement pis = connection.prepareStatement(query);
+                ResultSet resultSet = pis.executeQuery()) {
+            while (resultSet.next()) {
+                Resident resident = new Resident();
+
+                resident.setResidentId(resultSet.getInt("ResidentID"));
+                resident.setName(resultSet.getString("Name"));
+                resident.setFName(resultSet.getString("FName"));
+                resident.setGFName(resultSet.getString("GFName"));
+                resident.setDOB(resultSet.getString("DOB"));
+                resident.setPOB(resultSet.getString("POB"));
+                resident.setPhoneNumber(resultSet.getString("PhoneNumber"));
+                resident.setMName(resultSet.getString("MotherName"));
+                resident.setSex(resultSet.getString("Sex"));
+                resident.setCitizenship(resultSet.getString("Citizenship"));
+                resident.setMaritalStatus(resultSet.getString("MaritalStatus"));
+                resident.setJob(resultSet.getString("Job"));
+                resident.setBType(resultSet.getString("BloodType"));
+                resident.setHouseNumber(resultSet.getString("HouseNumber"));
+                resident.setECF(resultSet.getString("ECF"));
+                resident.setECP(resultSet.getString("ECP"));
+
+                residents.add(resident);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return residents;
+    }
+
+    @Override
+    public Resident getResidentByID(int rid) throws SQLException {
+        Resident resident = new Resident();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM Resident WHERE ResidentID = ?";
+        try (PreparedStatement pis = connection.prepareStatement(query);) {
+            pis.setInt(1, rid);
+
+            ResultSet resultSet = pis.executeQuery();
+            while (resultSet.next()) {
+                resident.setResidentId(resultSet.getInt("ResidentID"));
+                resident.setName(resultSet.getString("Name"));
+                resident.setFName(resultSet.getString("FName"));
+                resident.setGFName(resultSet.getString("GFName"));
+                resident.setDOB(resultSet.getString("DOB"));
+                resident.setPOB(resultSet.getString("POB"));
+                resident.setPhoneNumber(resultSet.getString("PhoneNumber"));
+                resident.setMName(resultSet.getString("MotherName"));
+                resident.setSex(resultSet.getString("Sex"));
+                resident.setCitizenship(resultSet.getString("Citizenship"));
+                resident.setMaritalStatus(resultSet.getString("MaritalStatus"));
+                resident.setJob(resultSet.getString("Job"));
+                resident.setBType(resultSet.getString("BloodType"));
+                resident.setHouseNumber(resultSet.getString("HouseNumber"));
+                resident.setECF(resultSet.getString("ECF"));
+                resident.setECP(resultSet.getString("ECP"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resident;
+    }
+
 }
