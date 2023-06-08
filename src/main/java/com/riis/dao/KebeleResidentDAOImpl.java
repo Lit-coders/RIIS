9fc0bcbc58a46d0e6a0045a97e25dfd4f5572ca8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-08 09:18:26 +0300

    create data access layer for the KebeleResident model and table

---
+++ src/main/java/com/riis/dao/KebeleResidentDAOImpl.java
@@ -0,0 +1,180 @@
+package com.riis.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.riis.database.DatabaseConnection;
+import com.riis.model.databasemodel.KebeleResident;
+import com.riis.model.databasemodel.Resident;
+
+public class KebeleResidentDAOImpl implements KebeleResidentDAO {
+    @Override
+    public List<KebeleResident> getAllKebeleResidents() throws SQLException {
+        List<KebeleResident> kebeleResidents = new ArrayList<>();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM KebeleResident";
+        try (PreparedStatement pis = connection.prepareStatement(query);
+                ResultSet resultSet = pis.executeQuery()) {
+            while (resultSet.next()) {
+                KebeleResident kebeleResident = new KebeleResident();
+
+                kebeleResident.setResidentId(resultSet.getInt("ResidentID"));
+                kebeleResident.setIdGivenDate(resultSet.getString("GivenDate"));
+                kebeleResident.setIdExpDate(resultSet.getString("ExpDate"));
+                kebeleResident.setExpStatus(resultSet.getInt("ExpirationStatus"));
+
+                kebeleResidents.add(kebeleResident);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return kebeleResidents;
+    }
+
+    @Override
+    public KebeleResident getKebeleResidentByKID(int kid) throws Exception {
+        KebeleResident kebeleResident = new KebeleResident();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM KebeleResident WHERE KRID = ?";
+        try (PreparedStatement pis = connection.prepareStatement(query)) {
+            pis.setInt(1, kid);
+            ResultSet resultSet = pis.executeQuery();
+            while(resultSet.next()) {
+                kebeleResident.setResidentId(resultSet.getInt("ResidentID"));
+                kebeleResident.setIdGivenDate(resultSet.getString("GivenDate"));
+                kebeleResident.setIdExpDate(resultSet.getString("ExpDate"));
+                kebeleResident.setExpStatus(resultSet.getInt("ExpirationStatus"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return kebeleResident;
+    }
+
+    @Override
+    public KebeleResident getKebeleResidentByRID(int rid) throws SQLException {
+        KebeleResident kebeleResident = new KebeleResident();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM KebeleResident WHERE ResidentID = ?";
+        try (PreparedStatement pis = connection.prepareStatement(query)) {
+            pis.setInt(1, rid);
+            ResultSet resultSet = pis.executeQuery();
+            while (resultSet.next()) {
+                kebeleResident.setResidentId(resultSet.getInt("ResidentID"));
+                kebeleResident.setIdGivenDate(resultSet.getString("GivenDate"));
+                kebeleResident.setIdExpDate(resultSet.getString("ExpDate"));
+                kebeleResident.setExpStatus(resultSet.getInt("ExpirationStatus"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return kebeleResident;
+    }
+
+    @Override
+    public Resident getKebeleResidentByID(int kid) throws SQLException {
+        ResidentDAO residentDAO = new ResidentDAOImpl();
+        Resident resident = new Resident();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM KebeleResident WHERE KRID = ?";
+        try (PreparedStatement pis = connection.prepareStatement(query);
+                ResultSet resultSet = pis.executeQuery()) {
+            while (resultSet.next()) {
+                int RID = resultSet.getInt("ResidentID");
+                resident = residentDAO.getResidentByID(RID);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resident;
+    }
+
+    @Override
+    public boolean checkIdExpirationStatus(int rid) throws SQLException {
+        boolean status = false;
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT ExpirationStatus FROM KebeleResident WHERE ResidentID = ?";
+        try (PreparedStatement pis = connection.prepareStatement(query)) {
+            pis.setInt(1, rid);
+            ResultSet resultSet = pis.executeQuery();
+            while (resultSet.next()) {
+                if (resultSet.getInt("ExpirationStatus") == 1) {
+                    status = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    @Override
+    public boolean checkIfRequestExists(int rid) throws SQLException {
+        Connection connection = DatabaseConnection.getInstance();
+        boolean status = false;
+        String query = "SELECT RequestType FROM Request WHERE RID = ?";
+        try (PreparedStatement pis = connection.prepareStatement(query);) {
+            pis.setInt(1, rid);
+            ResultSet resultSet = pis.executeQuery();
+            while (resultSet.next()) {
+                status = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    @Override
+    public void addKebeleResident(int id) throws SQLException {
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "INSERT INTO KebeleResident (ResidentID, GivenDate, ExpDate, ExpirationStatus) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement pis = connection.prepareStatement(query)) {
+            pis.setInt(1, id);
+
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = currentDateTime.format(formatter);
+
+            pis.setString(2, formattedDateTime);
+            pis.setString(3, currentDateTime.plusYears(3).format(formatter));
+
+            pis.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<KebeleResident> getKebeleResidentsByToken(String token) throws SQLException {
+        List<KebeleResident> kebeleResidents = new ArrayList<>();
+        Connection connection = DatabaseConnection.getInstance();
+        String query = "SELECT * FROM KebeleResident " +
+                       "INNER JOIN Resident ON KebeleResident.ResidentID = Resident.ResidentID " +
+                       "WHERE Resident.Name LIKE ? OR Resident.FName LIKE ? OR Resident.GFName LIKE ?";
+        try (PreparedStatement pis = connection.prepareStatement(query)) {
+            pis.setString(1, "%" + token + "%");
+            pis.setString(2, "%" + token + "%");
+            pis.setString(3, "%" + token + "%");
+            ResultSet resultSet = pis.executeQuery();
+            while (resultSet.next()) {
+                KebeleResident kebeleResident = new KebeleResident();
+                kebeleResident.setResidentId(resultSet.getInt("ResidentID"));
+                kebeleResident.setIdGivenDate(resultSet.getString("GivenDate"));
+                kebeleResident.setIdExpDate(resultSet.getString("ExpDate"));
+                kebeleResident.setExpStatus(resultSet.getInt("ExpirationStatus"));
+                kebeleResidents.add(kebeleResident);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return kebeleResidents;
+    }
+    
+}
