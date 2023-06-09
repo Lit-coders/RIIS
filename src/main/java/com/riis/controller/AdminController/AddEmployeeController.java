26de915e7dd324cbc815cc92237b0db8a2eff68e
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-06-09 09:09:39 -0700

    add combo box contents

--- src/main/java/com/riis/controller/AdminController/AddEmployeeController.java
+++ src/main/java/com/riis/controller/AdminController/AddEmployeeController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -41,14 +42,22 @@ public class AddEmployeeController implements Controller {
     private Button clear_btn;
 
     @FXML
-    private TextField job_field;
-
+    private ComboBox<String> job;
     @FXML
     private TextField pass_field;
 
     @FXML
     public void initialize() {
         Platform.runLater(Fname_field::requestFocus);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                job.getItems().addAll("System Administrator", "Kebele Manger", "Finance Officer",
+                        "Information Officer");
+            }
+        });
+        // job.setStyle(
+                // "-fx-background-color:white;-fx-border-radius:20px;-fx-border-color:#cacaca;-fx-border-width:1.5px;");
 
     }
 
@@ -56,20 +65,20 @@ public class AddEmployeeController implements Controller {
     void addEmployee(ActionEvent event) throws SQLException {
         AnchorPane pane = (AnchorPane) (clear_btn).getParent();
         VBox box = (VBox) pane.getChildren().get(1);
-        
+
         EmployeeDAO employeeDAO = new EmployeeDAOImpl();
         Employee employee = new Employee();
-        
+
         employee.setFirstName(Fname_field.getText());
         employee.setLastName(Lname_field.getText());
         employee.setMiddleName(Mname_field.getText());
-        employee.setUserName(Uname_field.getText());    
+        employee.setUserName(Uname_field.getText());
         employee.setPassword(pass_field.getText());
-        employee.setJob(job_field.getText());
+        employee.setJob(job.getValue());
 
-        if(!isEmpty(box) && isValid(box) && !isRepeated(box) && isStrong(box)){
+        if (!isEmpty(box) && isValid(box) && !isRepeated(box) && isStrong(box)) {
             boolean isAdded = employeeDAO.addEmployee(employee);
-            if(isAdded){
+            if (isAdded) {
                 JAlert alert = new JAlert("Success", "Employee Added Successfully!");
                 alert.showAlert();
                 clearAllFields(box);
@@ -82,7 +91,7 @@ public class AddEmployeeController implements Controller {
 
     private boolean isStrong(VBox box) {
         TextField field = (TextField) box.getChildren().get(9);
-        if(field.getText().length() < 5){
+        if (field.getText().length() < 5) {
             alertMessage("Password Length must be greater than 5.");
             field.requestFocus();
             return false;
@@ -96,7 +105,7 @@ public class AddEmployeeController implements Controller {
         EmployeeDAO employeeDAO = new EmployeeDAOImpl();
 
         Employee employee = employeeDAO.getEmployeeByUsername(field.getText());
-        if(employee.getUserName() != null) {
+        if (employee.getUserName() != null) {
             alertMessage("Username already exists!");
             field.requestFocus();
             return true;
@@ -105,9 +114,9 @@ public class AddEmployeeController implements Controller {
     }
 
     private boolean isEmpty(VBox box) {
-        for(int i=1; i<box.getChildren().size(); i += 2){
+        for (int i = 1; i < box.getChildren().size()-1; i += 2) {
             TextField field = (TextField) box.getChildren().get(i);
-            if(field.getText().isBlank()){
+            if (field.getText().isBlank()) {
                 alertMessage("You have Empty Fields!");
                 field.requestFocus();
                 return true;
@@ -117,22 +126,22 @@ public class AddEmployeeController implements Controller {
     }
 
     private boolean isValid(VBox box) {
-        for(int i=1; i<box.getChildren().size(); i += 2){
+        for (int i = 1; i < box.getChildren().size()-1; i += 2) {
             TextField field = (TextField) box.getChildren().get(i);
-            for(int j=0; j<field.getText().length(); j++){
-                if(field.getText().charAt(j) == '\'' || field.getText().charAt(j) == '"'){
+            for (int j = 0; j < field.getText().length(); j++) {
+                if (field.getText().charAt(j) == '\'' || field.getText().charAt(j) == '"') {
                     alertMessage("Using Quotation marks is not allowed!");
                     field.requestFocus();
                     return false;
                 }
             }
             // if(i == 1 || i == 3 || i == 5 || i == 11){
-            //     String alphaPattern = "^[a-zA-Z\\s]*$";
-            //     if(field.getText().matches(alphaPattern)){
-            //         Data.alertMessage("Error", "You have inserted invalid data!");
-            //         field.requestFocus();
-            //         return false;
-            //     }
+            // String alphaPattern = "^[a-zA-Z\\s]*$";
+            // if(field.getText().matches(alphaPattern)){
+            // Data.alertMessage("Error", "You have inserted invalid data!");
+            // field.requestFocus();
+            // return false;
+            // }
             // }
         }
         return true;
@@ -144,9 +153,9 @@ public class AddEmployeeController implements Controller {
         VBox box = (VBox) pane.getChildren().get(1);
         clearAllFields(box);
     }
-    
-    void clearAllFields(VBox box){
-        for(int i=1; i<box.getChildren().size(); i += 2){
+
+    void clearAllFields(VBox box) {
+        for (int i = 1; i < box.getChildren().size()-1; i += 2) {
             TextField field = (TextField) box.getChildren().get(i);
             field.clear();
         }
@@ -154,36 +163,33 @@ public class AddEmployeeController implements Controller {
         field.requestFocus();
     }
 
-    public void alertMessage(String message){
-        JAlert alert = new JAlert("Error",message);
+    public void alertMessage(String message) {
+        JAlert alert = new JAlert("Error", message);
         alert.showAlert();
     }
 
     @FXML
-    void handleKeyPress (KeyEvent event){
+    void handleKeyPress(KeyEvent event) {
         TextField currentField = (TextField) event.getSource();
         VBox box = (VBox) currentField.getParent();
         int totalSize = box.getChildren().size();
         int currentIndex = box.getChildren().indexOf(currentField);
-        if (event.getCode() == KeyCode.ENTER){
-            if (currentIndex < totalSize-2){
-                box.getChildren().get(currentIndex+2).requestFocus();
-            }
-            else{
+        if (event.getCode() == KeyCode.ENTER) {
+            if (currentIndex < totalSize - 2) {
+                box.getChildren().get(currentIndex + 2).requestFocus();
+            } else {
                 approve_btn.fire();
             }
 
-        } else if (event.getCode() == KeyCode.BACK_SPACE){
-            if (currentIndex >=2 && currentField.getText().isEmpty()){
-                box.getChildren().get(currentIndex-2).requestFocus();
-            } 
-            else{
+        } else if (event.getCode() == KeyCode.BACK_SPACE) {
+            if (currentIndex >= 2 && currentField.getText().isEmpty()) {
+                box.getChildren().get(currentIndex - 2).requestFocus();
+            } else {
                 box.getChildren().get(currentIndex).requestFocus();
             }
         }
 
     }
-
 
     @Override
     public void getView() throws Exception {
