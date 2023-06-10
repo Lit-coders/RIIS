66550e71db4a97c9492b9cf0f98b8d1fdd76b8dc
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-10 12:10:05 +0300

    add new model for the map holder and all its database implementations

---
+++ src/main/java/com/riis/model/databasemodel/MapHolder.java
@@ -0,0 +1,61 @@
+package com.riis.model.databasemodel;
+
+public class MapHolder {
+    private int mapHolderID;
+    private String mapHolderName;
+    private String mapHolderFName;
+    private String mapHolderGFName;
+    private String mapHolderPhoneNum;
+    private byte[] mapHolderPhoto;
+
+    public MapHolder() {
+    }
+
+    public int getMapHolderID() {
+        return mapHolderID;
+    }
+
+    public void setMapHolderID(int mapHolderID) {
+        this.mapHolderID = mapHolderID;
+    }
+
+    public String getMapHolderName() {
+        return mapHolderName;
+    }
+
+    public void setMapHolderName(String mapHolderName) {
+        this.mapHolderName = mapHolderName;
+    }
+
+    public String getMapHolderFName() {
+        return mapHolderFName;
+    }
+
+    public void setMapHolderFName(String mapHolderFName) {
+        this.mapHolderFName = mapHolderFName;
+    }
+
+    public String getMapHolderGFName() {
+        return mapHolderGFName;
+    }
+
+    public void setMapHolderGFName(String mapHolderGFName) {
+        this.mapHolderGFName = mapHolderGFName;
+    }
+
+    public String getMapHolderPhoneNum() {
+        return mapHolderPhoneNum;
+    }
+
+    public void setMapHolderPhoneNum(String mapHolderPhoneNum) {
+        this.mapHolderPhoneNum = mapHolderPhoneNum;
+    }
+
+    public byte[] getMapHolderPhoto() {
+        return mapHolderPhoto;
+    }
+
+    public void setMapHolderPhoto(byte[] mapHolderPhoto) {
+        this.mapHolderPhoto = mapHolderPhoto;
+    }
+}
